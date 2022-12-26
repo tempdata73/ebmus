8a025153dc3dcc935d44cf2c602031309447ca87
Iñaki Liendo <sebastian@liendo.net>
2022-12-26 17:43:49 -0600

    basic propotype

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,50 @@ impl ToString for DefaultTemplate {
 }
 
 // lib.rs
+#[derive(Debug)]
 pub struct Puzzle {
     template: String,
     nonce: usize,
+    pub difficulty: usize,
 }
 
 impl Puzzle {
-    pub fn new(template: String) -> Self {
-        Puzzle { template, nonce: 0 }
+    pub fn new(template: &str, difficulty: usize) -> Self {
+        Puzzle {
+            template: template.to_owned(),
+            nonce: 0,
+            difficulty,
+        }
     }
 
-    pub fn is_solved(&self) -> bool {
-        todo!();
+    fn is_solved(&self) -> bool {
+        let dump = self.hash();
+        let mut dump = dump.chars();
+
+        for _ in 0..self.difficulty {
+            if dump.next().unwrap() != '0' {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    pub fn solve(&mut self) {
+        while !self.is_solved() {
+            self.nonce += 1;
+        }
+    }
+
+    pub fn hash(&self) -> String {
+        let stream = self.to_string();
+        let dump = blake3::hash(stream.as_bytes());
+        dump.to_string()
     }
 }
 
-fn solve(puzzle: &mut Puzzle, difficulty: usize) {
-    todo!();
+impl ToString for Puzzle {
+    fn to_string(&self) -> String {
+        format!("{}:{}", self.template, self.nonce)
+    }
 }
