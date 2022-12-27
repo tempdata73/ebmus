ec956e516d5b01fd2e99e0e782806a0b085371e7
Iñaki Liendo <sebastian@liendo.net>
2022-12-27 13:03:43 -0600

    remove difficulty as property

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,27 @@
 #[cfg(feature = "template")]
 pub mod template;
 
-// lib.rs
+const MAX_DIFFICULTY: u8 = 64;
+
 #[derive(Debug)]
 pub struct Puzzle {
     template: String,
-    nonce: usize,
-    difficulty: u8,
+    pub nonce: usize,
 }
 
 impl Puzzle {
-    pub fn new(template: String, difficulty: u8) -> Self {
-        let mut puzzle = Puzzle {
-            template,
+    pub fn new(template: &str) -> Self {
+        Puzzle {
+            template: template.to_string(),
             nonce: 0,
-            difficulty: 0,
-        };
-
-        puzzle.set_difficulty(difficulty).unwrap();
-        puzzle
+        }
     }
 
-    fn is_solved(&self) -> bool {
+    fn is_solved(&self, difficulty: u8) -> bool {
         let dump = self.hash();
         let mut dump = dump.chars();
 
-        for _ in 0..self.difficulty {
+        for _ in 0..difficulty {
             if dump.next().unwrap() != '0' {
                 return false;
             }
@@ -34,25 +30,22 @@ impl Puzzle {
         true
     }
 
-    pub fn solve(&mut self) {
-        while !self.is_solved() {
+    pub fn solve(&mut self, difficulty: u8) -> Result<(), &'static str> {
+        if difficulty > MAX_DIFFICULTY {
+            return Err("Difficulty cannot be greater than 64.");
+        }
+
+        while !self.is_solved(difficulty) {
             self.nonce += 1;
         }
+
+        Ok(())
     }
 
     pub fn hash(&self) -> String {
         let stream = self.to_string();
         let dump = blake3::hash(stream.as_bytes());
         dump.to_string()
-    }
-
-    pub fn set_difficulty(&mut self, difficulty: u8) -> Result<(), &'static str> {
-        if difficulty > 64 {
-            return Err("Difficulty cannot be greater than 64.");
-        }
-
-        self.difficulty = difficulty;
-        Ok(())
     }
 }
 
